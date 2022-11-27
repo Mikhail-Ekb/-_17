aa4b0865de4dc59542a6575a8ba4e0eb9ff81b13
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-28 00:15:20 +0500

    Добавьте файлы проекта.

---
+++ Задание_17/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Задание_17
+{
+    internal class Account<T>
+    {
+        private T Num { get; set; }
+        private double Balance { get; set; }
+        private string Name { get; set; }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите имя");
+            Name = Console.ReadLine();
+        }
+
+        public string GetInfo()
+        {
+            return $"Клиент {Name}\nНомер счета {Num}\nБаланс {Balance}";
+        }
+    }
+}
